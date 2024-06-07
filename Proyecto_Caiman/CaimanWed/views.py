0c500544af9a40a2a0bf580355c929feb38aebc5
Memonsteer <gu.saavedrag@duocuc.cl>
2024-06-07 16:07:54 -0400

    commit home guillermo

--- Proyecto_Caiman/CaimanWed/views.py
+++ Proyecto_Caiman/CaimanWed/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from .models import Producto, Transportista, OrdenDespacho, DetalleOrden, Supermercado, Envio, Usuario
 
-def index(request):
-    return render(request, 'index.html')
-
-# Define otras vistas según tus necesidades
+def home(request):
+    return render(request, 'home.html')
