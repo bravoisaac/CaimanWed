72296e5bfcbf13fef3db551261faf81dbeb398f0
cesar <ce.puentes@duocuc.cl>
2024-06-07 15:45:07 -0400

    base de datos
    primera base de datos (para cambios)

--- Proyecto_Caiman/CaimanWed/views.py
+++ Proyecto_Caiman/CaimanWed/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Producto, Transportista, OrdenDespacho, DetalleOrden, Supermercado, Envio, Usuario
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+# Define otras vistas según tus necesidades
