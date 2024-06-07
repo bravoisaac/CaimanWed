efe67272d609c8d611ed54853c419ecb0f77c89e
eduardo <ed.ulloah@duocuc.cl>
2024-06-07 16:33:12 -0400

    guardando cambios

--- Proyecto_Caiman/CaimanWed/urls.py
+++ Proyecto_Caiman/CaimanWed/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path 
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('registro/', views.registro, name='registro'),
+]
--- Proyecto_Caiman/CaimanWed/views.py
+++ Proyecto_Caiman/CaimanWed/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegistroForm
 # Create your views here.
+
+def registro(request):   
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+           
+            return redirect('index') 
+    else:
+        form = RegistroForm()
+    return render(request, 'registro.html', {'form': form})
+    
+
+def index(request):
+    return render (request,'index.html')
