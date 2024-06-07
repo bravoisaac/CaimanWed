b5fc2e31ce595d127e5b9854054575b654b6dbee
Memonsteer <gu.saavedrag@duocuc.cl>
2024-06-07 16:38:57 -0400

    commit con varias cosas agregadas al home

--- Proyecto_Caiman/CaimanWed/views.py
+++ Proyecto_Caiman/CaimanWed/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def store(request):
+    return render(request, 'store.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def support(request):
+    return render(request, 'support.html')
